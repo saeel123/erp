6c809f32fa1d4ededf7624d441a8089513e9bdd4
saeel123 <saeel@dccper.com>
2017-06-13 17:54:20 +0530

    Updated database

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/meanauth',
+  //database: 'mongodb://localhost:27017/meanauth',
+  database: 'mongodb://saeel:saeel123@ds123722.mlab.com:23722/erp-dev',
+
   secret: 'mysecret'
 }
